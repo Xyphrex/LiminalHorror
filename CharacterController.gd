e6f5676ae9c187e71b2a43652be9077adb95830a
Justin <jhoogwaerts@gmail.com>
2023-04-08 16:04:14 +1000

    Added Lighting

--- CharacterController.gd
+++ CharacterController.gd
@@ -1,16 +1,20 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var Head = $character_head
 @export var sensitivity = 0.2
+@export var sprint = 10
+@export var SPEED = 5.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _process(delta):
+	$character_head/character_head_eyes/GUI/FPS_Counter.text = "FPS: " + str(1/delta)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,8 +30,13 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		if Input.is_key_pressed(KEY_SHIFT):
+			velocity.x = direction.x * (SPEED + sprint)
+			velocity.z = direction.z * (SPEED + sprint)
+		else:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
